aa1469b18f0723b85c1b4d6e5ecf575486582c6d
MarvinNL046 <marvinsmit1988@gmail.com>
2026-04-23 22:27:30 +0200

    Add Travelpayouts Drive script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Travelpayouts Drive */}
+        <script
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          dangerouslySetInnerHTML={{
+            __html: `(function(){var s=document.createElement("script");s.async=1;s.src="https://emrldco.com/NTIxOTMx.js?t=521931";document.head.appendChild(s);})();`,
+          }}
+        />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
